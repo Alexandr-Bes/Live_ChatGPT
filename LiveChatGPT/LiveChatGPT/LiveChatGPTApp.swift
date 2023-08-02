8a16eaf34f270df2916028772431209082824106
Alex Bezkopylnyi <sanok.bes@gmail.com>
2023-08-02 18:24:06 +0100

    Initial SwiftUI project setup

---
+++ LiveChatGPT/LiveChatGPT/LiveChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveChatGPTApp.swift
+//  LiveChatGPT
+//
+//  Created by AlexBezkopylnyi on 02.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LiveChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
